cd475dce7d2bda7f8033f3eebf3d58c5b0f981b2
Dyego Vincer Montero <monterodyego@gmail.com>
2026-09-01 22:54:02 +0800

    Diego@DESKTOP-3D4UU3C MINGW64 /c/1a (master) $ git status On branch master
    No commits yet
    
    Changes to be committed:
      (use "git rm --cached <file>..." to unstage)
            new file:   Assignment3.java/.gitignore
            new file:   Assignment3.java/.idea/.gitignore
            new file:   Assignment3.java/.idea/misc.xml
            new file:   Assignment3.java/.idea/modules.xml
            new file:   Assignment3.java/Assignment3.java.iml
            new file:   Assignment3.java/src/Main.java
    
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    .git/COMMIT_EDITMSG [unix] (22:51 01/09/2026)                                              1,0-1 All
    "/c/1a/.git/COMMIT_EDITMSG" [unix] 16L, 489B

---
+++ Assignment3.java/src/Main.java
@@ -0,0 +1,25 @@
+public class Main
+{
+    public static void main(String[] args) {
+        /**
+         * Assign user information to variables
+         */
+        String firstName = "Dyego Vincer";
+        String lastName = "Montero";
+        String address = "Pozorrubio";
+        String mobileNumber = "0951 684 3062";
+        int age = 22;
+        double gwa = 90;
+
+        /*
+         * Display output
+         */
+        System.out.println("Firstname :   " + firstName);
+        System.out.println("Lastname :   " + lastName);
+        System.out.println("Address :   " + address);
+        System.out.println("Mobile Number :   " + mobileNumber);
+        System.out.println("Age :   " + age);
+        System.out.println("GWA :   " + gwa);
+    }
+}
+
