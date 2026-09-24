27afe9bc8adf00785db56d8e3f98e837e97ff146
monterodyego-png <monterodyego@gmail.com>
2026-09-24 09:45:29 +0800

    add files via upload

---
+++ firstJava.java/src/firstJava.java
@@ -0,0 +1,11 @@
+public class firstJava{
+    public static void main(String[]Args){
+        String firstName = "Dyego Vincer ";
+        String middleName = "Malala ";
+        String lastName = "Montero";
+        String section = "BSIT-1A";
+
+        System.out.println (firstName + middleName +lastName);
+        System.out.println (section );
+    }
+}
